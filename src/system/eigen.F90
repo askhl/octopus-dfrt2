cfe08fc39c123e77902625f824b9be6d61e70aed
umberto <umbe@portabilix.local>
2012-05-10 11:41:21 +0200

    - fixed some git inconsistencies (I must have done something weird before)

--- src/system/eigen.F90
+++ src/system/eigen.F90
@@ -94,7 +94,6 @@ module eigensolver_m
        RS_LOBPCG  =  8,         &
        RS_RMMDIIS = 10,         &
        RS_ARPACK  = 12,         &  
-       RS_BICG    = 13,         &
        RS_DIRECT  = 14
 
 contains
@@ -186,7 +185,6 @@ contains
     case(RS_MG)
     case(RS_CG)
     case(RS_PLAN)
-    case(RS_BICG)
     case(RS_DIRECT)
     case(RS_EVO)
       !%Variable EigensolverImaginaryTime
@@ -285,7 +283,7 @@ contains
     end if
     
     select case(eigens%es_type)
-    case(RS_PLAN, RS_CG, RS_BICG, RS_DIRECT, RS_LOBPCG, RS_RMMDIIS)
+    case(RS_PLAN, RS_CG, RS_DIRECT, RS_LOBPCG, RS_RMMDIIS)
       call preconditioner_init(eigens%pre, gr)
     case default
       call preconditioner_null(eigens%pre)
@@ -314,7 +312,7 @@ contains
     call subspace_end(eigens%sdiag)
 
     select case(eigens%es_type)
-    case(RS_PLAN, RS_CG, RS_BICG, RS_LOBPCG, RS_RMMDIIS)
+    case(RS_PLAN, RS_CG, RS_LOBPCG, RS_RMMDIIS)
       call preconditioner_end(eigens%pre)
     end select
 
@@ -416,8 +414,6 @@ contains
           call zeigensolver_cg2_new(gr, st, hm, eigens%tolerance, maxiter, eigens%converged(ik), ik, eigens%diff(:, ik))
         case(RS_CG)
           call zeigensolver_cg2(gr, st, hm, eigens%pre, eigens%tolerance, maxiter, eigens%converged(ik), ik, eigens%diff(:, ik))
-        case(RS_BICG)
-           call eigensolver_bicg(gr, st, hm, eigens%pre, eigens%tolerance, maxiter, eigens%converged(ik), ik, eigens%diff(:, ik))
         case(RS_DIRECT)
            call eigensolver_direct(gr, st, hm, eigens%pre, eigens%tolerance, maxiter, eigens%converged(ik), ik, eigens%diff(:, ik))
         case(RS_PLAN)
@@ -462,7 +458,7 @@ contains
     ! Moreover the eigenvalues ordering need to be imposed as there is no eigensolver 
     ! supporting this ordering (yet).
     if(hm%cmplxscl) then !cmplxscl
-!       call states_orthogonalize_cproduct(st, gr%mesh)
+      call states_orthogonalize_cproduct(st, gr%mesh)
       call states_sort_complex(st, gr%mesh)
     end if
     
